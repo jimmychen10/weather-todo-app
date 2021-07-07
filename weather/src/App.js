27876e14b2443d20e744b114f7d2def337ada4a7
Jimmy Chen <jimmychen1996@yahoo.com>
2021-07-07 02:56:34 -0700

    Implemented find userlocation button

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,57 +1,91 @@
-import React,{useState,useEffect} from "react"
-import axios from "axios"
+import WeatherDisplay from "./Components/WeatherDisplay.js"
+import LoadingIndicator from "./Components/LoadingIndicator.js"
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Styles/Weather.css"
-import {Form,Button} from 'react-bootstrap';
 import "./Styles/App.css"
-import WeatherDisplay from "./Components/WeatherDisplay.js"
-import UserLocation from './Components/UserLocation.js'
-// import {geolocated} from 'react-geolocated';
+
+import {Form,Button} from 'react-bootstrap';
+import { BsGeoAlt } from 'react-icons/bs';
+
+import React,{useState,useEffect} from "react"
+import axios from "axios"
 
 
 export default function App(){
-  // const weatherId = "9db9a4d3ba51b8e5bcb97f497a271e58"
+  // const weatherId = "9db9a4d3ba51b8e5bcb97f497a271e58" for OpenWeather
+
+  let wantLogLan = false
+  let loadingApi = false
+  let lon,lan;
   const weatherId ="d733b204b23441d5934886d9e788969e"
-  const [loadingApi, setLoadingApi] = useState(true)
-  const [weather,setWeather] = useState()
+
   const [city, setCity] = useState()
   const [currentCity,setCurrentCity] = useState(city)
   const [country,setCountry] = useState()
   const [currentCountry,setCurrentCountry] = useState()
-  const [weatherData,setWeatherData] = useState({})
+  const [weatherData,setWeatherData] = useState("")
 
 
-  function getApiData(){
-    const apiData = axios.get("https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=7&city="+city+"&country="+country+"&key="+weatherId)
+  async function getApiData(){
 
-    if ( apiData.ok ){
-        console.log("throw error works")
-        throw new ('HTTP error! status: ', apiData)
+    let apiData;
+    try {
+      const temp = (country === undefined ? "": "&country="+country)
+      if(wantLogLan){
+        apiData = await axios.get("https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=7&lat="+lan+"&lon="+lon+"&key="+weatherId)
+        wantLogLan = false
       }
-      else{         
-        return  apiData      
+      else{
+        apiData = await axios.get("https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=7&city="+city+temp+"&key="+weatherId)
+        console.log("want City")
       }
+      loadingApi = false
+      return  apiData     
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-   function getWeatherData(){
-      setCountry("")
-      
-      getApiData().then( blob =>{
-        setWeatherData(blob.data)
-        setCurrentCity(blob.data.city_name)
-        
-        setCurrentCountry(blob.data.country_code)
-        // setWeather(blob.data.data[0].weather.description) 
-        setLoadingApi(false)
+  function getUserLocation(){
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    function success(pos) {
+
+      loadingApi = true
+      lon = pos.coords.longitude
+      lan = pos.coords.latitude
+      getWeatherData()
+    }
+    
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    navigator.geolocation.getCurrentPosition(success,error,options)
+    wantLogLan = true
+  }
+
+   async function getWeatherData(){
+      try {
+        const data = await getApiData()
+        if(loadingApi === false){
+          setWeatherData(data.data)
+          setCurrentCity(data.data.city_name)
+          setCurrentCountry(data.data.country_code)
+        }
+
+      } catch (error) {
+        console.log(error)
       }
-    )  
 
   }
 
 
   useEffect(()=>{
     console.log("Mounted")
-    getWeatherData()
     return () => {
       console.log("cleaned up");
     };
@@ -60,21 +94,10 @@ export default function App(){
 
   function handleChange(e){
     e.preventDefault()
+    loadingApi = true
     getWeatherData()
     console.group(e)
   }
-
-  
-if(loadingApi){
-  return (
-    <div className="backGround" >
-      <div className = "loading"> 
-        <h1>Loading</h1> 
-      </div>
-  </div>
-  )
-}
-else{
 
    return(
     
@@ -92,8 +115,9 @@ else{
             <Form.Control className=" form" type="text" placeholder="Enter City" value = {city} onChange ={e => setCity(e.target.value)} required/>
             <Form.Control className=" form" type="text" placeholder="Optional: Enter Country" value = {country}  onChange ={e => setCountry(e.target.value)}/>
             
-            <Button variant="primary" type="submit" className="">
-            Submit
+            <Button variant="primary" type="submit" >Submit</Button>
+            <Button variant="secondary"  onClick={getUserLocation}>
+              <BsGeoAlt/>
             </Button>
 
             </Form.Row>
@@ -101,20 +125,16 @@ else{
           </Form>
           </span>
           </div>
+          
     <div className = "displayWeather">
-    { weatherData !== "" &&
-      
-      <WeatherDisplay weatherData = {weatherData}/>
     
-    }
+
+    {loadingApi ?<LoadingIndicator type="bubbles" color = "grey"/> :weatherData !== "" && <WeatherDisplay weatherData = {weatherData}/>}
 
     </div>
-
-     
-      {/* <UserLocation/> */}
 
     </div>
   )
     }
 
-}
+// }
